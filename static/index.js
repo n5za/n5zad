d07aa77f8554e404f627aab9ad7897d6c486cf1a
NS2 <nasa96128@gmail.com>
2026-07-27 04:04:25 +0000

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,17 +1,4 @@
-/* global Element */
 
-/**
- *  The Annoying Site
- *  https://theannoyingsite.com
- *
- *  Author:
- *    Feross Aboukhadijeh
- *    https://feross.org
- *
- *  Patreon:
- *    If you enjoyed this, please support me on Patreon!
- *    https://www.patreon.com/feross
- */
 
 const SCREEN_WIDTH = window.screen.availWidth
 const SCREEN_HEIGHT = window.screen.availHeight
